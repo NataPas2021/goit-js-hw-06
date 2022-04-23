e0918fa5b350c3abeb0d23be6202509ed3b68232
NataPas2021 <94239256+NataPas2021@users.noreply.github.com>
2022-04-23 04:18:26 +0300

    added basic

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+//const categoriesListRef = document.querySelectorAll('#categories');
+//console.log('Number of categories', categoriesListRef.length);
+
+
+const listOfItemsInCategoriesRef = document.querySelectorAll('.item');
+console.log('Number of categories:', listOfItemsInCategoriesRef.length);
+
+const titleOfCategoryEl = document.querySelector('.item');
+console.log('Category:', titleOfCategoryEl.textContent);
+
+
