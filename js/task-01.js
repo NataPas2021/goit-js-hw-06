4a994307819c7c80c30068b05bc13a0551b97b6e
NataPas2021 <94239256+NataPas2021@users.noreply.github.com>
2022-04-26 13:13:21 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -5,7 +5,6 @@
 const listOfItemsInCategoriesRef = document.querySelectorAll('.item');
 console.log('Number of categories:', listOfItemsInCategoriesRef.length);
 
-const titleOfCategoryEl = document.querySelector('.item');
-console.log('Category:', titleOfCategoryEl.textContent);
+
 
 
