77cfee8576829854d4e84192381e89a78f2e1208
NataPas2021 <94239256+NataPas2021@users.noreply.github.com>
2022-05-01 14:47:41 +0300

    task 1 ready

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,14 @@
 //console.log('Number of categories', categoriesListRef.length);
 
 
-const listOfItemsInCategoriesRef = document.querySelectorAll('.item');
-console.log('Number of categories:', listOfItemsInCategoriesRef.length);
+const numberOfCategoriesRef = document.querySelector('ul#categories');
+console.log('Number of categories:', numberOfCategoriesRef.children.length);
 
+const nameAndElementsInCategoryRef = numberOfCategoriesRef.querySelectorAll('li.item');
+nameAndElementsInCategoryRef.forEach((category) => {
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.lastElementChild.children.length}`);
+})
 
 
 
