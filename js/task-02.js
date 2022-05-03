af759f4560211cd7d702f63145ec5ce047033956
NataPas2021 <94239256+NataPas2021@users.noreply.github.com>
2022-05-03 14:24:29 +0300

    task 2 in progress

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientRef = ingredients.forEach((ingredient) => {
+  const itemRef = document.createElement('li');
+  itemRef.classList.add('item');
+  itemRef.textContent = `${ingredient}`;
+  console.log(itemRef);
+});
+
+const ingredientsRef = document.querySelector('ul#ingredients');
+ingredientsRef.appendChild(ingredientRef);
+
+console.log(ingredientsRef);
+
+
+
+//const listRef = document.getElementById('ingredients');
+//listRef.appendChild(ingredientsRef);
+
+
+
+//ingredients.appendChild(listItemRef);
+
+
+
+
