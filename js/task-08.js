ac76ac9a02b85026d8182eaa2f6985ad16f334fa
NataPas2021 <94239256+NataPas2021@users.noreply.github.com>
2022-05-16 20:58:27 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+ event.preventDefault();
+ const formRef = event.currentTarget.elements;
+ const email = formRef.email.value;
+ const password = formRef.password.value;
+ const formData = {email, password};
+ if(email === ''|| password === '') {
+     alert('All fields must be fill out!');
+ } else{
+     console.log({email, password});
+ };
+
+ console.log(formData);
+ 
+ event.currentTarget.reset();
+}
+
 
